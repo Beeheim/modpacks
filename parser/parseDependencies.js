76d1294e277a5e3b1c59bcccb06d9db71444f381
Cody Smith <csmith.su@gmail.com>
2024-01-13 22:52:11 +0000

    fetch and parse package index

--- parser/parseDependencies.js
+++ parser/parseDependencies.js
@@ -2,7 +2,7 @@ const { dependencies } = require('../vanilla-plus/optional-pack/manifest.json');
 const axios = require('axios');
 const rateLimit = require('axios-rate-limit');
 const chalk = require('chalk');
-const zlib = require('zlib');
+
 
 const package = 'https://valheim.thunderstore.io/api/experimental/package'; // This is the url for a single package
 const index = 'https://valheim.thunderstore.io/api/experimental/package-index'; // very large index. 160k entries;
@@ -16,40 +16,46 @@ async function parseDeps() {
             const parts = dependency.split("-");
             if (parts.length >= 2) {
                 return {
-                    author: parts[0],
-                    package: parts.slice(1, -1).join("-"),  // package name can contain "-"
-                    version: parts[parts.length - 1]
+                    namespace: parts[0],
+                    name: parts.slice(1, -1).join("-"),  // package name can contain "-"
+                    version_number: parts[parts.length - 1]
                 };
             } else {
                 console.log(chalk.red(`Error: Bad entry, parts.length too short\n${parts}`))
             }
         });
-      return parsedDependencies;
+        return parsedDependencies;
     } else {
         console.error(chalk.red(`Dependencies must be an array of strings`));
     }
 }
 
 async function fetchIndex(indexUrl) {
+    console.log(indexUrl);
     const thunderstore = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 1000 });
     const oneminute = 60000
-    let response = await thunderstore.get(indexUrl, {
-    respondeType: 'arraybuffer'
-    })
-    .then(res => {
-      zlib.gunzip(res.data, (err, decompressed) => {
-        if (err) {
-          console.log(chalk.red(`Error: ${err}`));
-        } else {
-          response = JSON.parse(decompressed.toString('utf8'));
+    const response = await thunderstore.get(indexUrl, {
+        headers: {
+            'Accept-Encoding': 'gzip'
         }
-      });
     })
-    .catch(error => {
-      console.error(error);
-    });
-  console.log(chalk.green(`Index fetched, ${response.data.length} entries`));
-  return response;
+        .then(res => {
+            console.log(chalk.green(`Index fetched, ${res.data.length} entries`));
+            let arr = res.data.split('\n').map(str => {
+                try {
+                    return JSON.parse(str);
+                } catch (error) {
+                    console.error(`Failed to parse JSON: ${str}`);
+                    return null;
+                }
+            }).filter(item => item !== null);
+            console.log(arr);
+            return arr;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    return response;
 }
 
 async function processDependencies(deps) {
@@ -57,7 +63,6 @@ async function processDependencies(deps) {
     const thunderstore = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 1000 });
     const rateLimitErrorCode = 429
     const oneminute = 60000
-    // console.log(deps);
     if (deps.length === 0) {
         console.log(chalk.red('deps is empty'));
         return;
@@ -65,15 +70,15 @@ async function processDependencies(deps) {
     for (const mod of deps) {
         try {
             const response = await thunderstore.get(
-        `${package}/${mod.author}/${mod.package}/`, {
+        `${package}/${mod.namespace}/${mod.name}/`, {
                 headers: {
                     'Accept': 'application/json'
                 }
             });
-            if (response.data.latest.version_number === mod.version) {
-                console.log(chalk.cyan(`${mod.package}: version matches: ${mod.version}`));
+            if (response.data.latest.version_number === mod.version_number) {
+                console.log(chalk.cyan(`${mod.name}: version matches: ${mod.version_number}`));
             } else {
-                console.log(chalk.yellow(`${mod.package}: new version available: ${mod.version} -> ${response.data.latest.version_number}`));
+                console.log(chalk.yellow(`${mod.name}: new version available: ${mod.version_number} -> ${response.data.latest.version_number}`));
             }
         } catch (error) {
             if (error.response && error.response.status === rateLimitErrorCode) {
@@ -86,24 +91,4 @@ async function processDependencies(deps) {
     }
 }
 
-//parseDeps().then(processDependencies);
-fetchIndex(index);
-
-
-// parsedDependencies.forEach(mod => {
-//     thunderstore.get(`https://valheim.thunderstore.io/api/experimental/package/${mod.author}/${mod.package}/`, {
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     })
-//         .then(response => {
-//             if (response.data.latest.version_number === mod.version) {
-//                 console.log(`${mod.package}: version matches: ${mod.version}`)
-//             } else {
-//                 console.log(`${mod.package}: new version available: ${mod.version} -> ${response.data.latest.version_number}`)
-//             }
-//         })
-//         .catch(error => {
-//             console.error(error);
-//         });
-// });
+fetchIndex(index).then(processDependencies);
